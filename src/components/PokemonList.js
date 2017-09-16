56d51126203ea830c04c3dbc01e7657c1ca1325c
Roberto MF <git@virgulilla.com>
2017-09-16 21:57:39 +0200

    Add pagination

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,12 +1,79 @@
 import React, { Component } from 'react'
 
+const { NUM_PKMN_BY_PAGE } = require('../config.json')
+
 class PokemonList extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      currentPage: 0,
+    }
+
+    this.getPrevPageButton.bind(this)
+    this.getNextPageButton.bind(this)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.store.length !== this.props.store.length) {
+      this.setState({
+        currentPage: 0,
+      })
+    }
+  }
+
+  lastPageNumber() {
+    return Math.ceil(this.props.store.length / NUM_PKMN_BY_PAGE) - 1
+  }
+  turnPage(numOfPages) {
+    this.setState((prevState) => {
+      return {
+        currentPage: prevState.currentPage + numOfPages
+      }
+    })
+  }
+  getPrevPageButton() {
+    const isDisabled = this.state.currentPage <= 0
+    return (
+      <button
+        onClick={() => this.turnPage(-1)}
+        disabled={isDisabled}>
+          previous page
+      </button>
+    )
+  }
+  getNextPageButton() {
+    const isDisabled = this.state.currentPage >= this.lastPageNumber()
+    return (
+      <button
+        onClick={() => this.turnPage(1)}
+        disabled={isDisabled}>
+          next page
+      </button>
+    )
+  }
+
+  onlyPokemonInCurrentPage() {
+    const pageStartIndex = this.state.currentPage * NUM_PKMN_BY_PAGE
+    const pageEndIndex = pageStartIndex + NUM_PKMN_BY_PAGE
+
+    return (pokemon, index) => {
+      return index >= pageStartIndex && index < pageEndIndex
+    }
+  }
+
   render() {
     return (
       <div>
-        {this.props.store.map((pokemon, index) => <p key={index}>{pokemon.name}</p> )}
+        <nav>
+          {this.getPrevPageButton()}
+          {this.getNextPageButton()}
+        </nav>
         <ul>
-          {this.props.store.map((pokemon, index) => <li key={index}>{pokemon.name}</li> )}
+          {this.props.store
+            .filter(this.onlyPokemonInCurrentPage())
+            .map((pokemon, index) => <li key={index}>{pokemon.name}</li>)
+          }
         </ul>
       </div>
     )
