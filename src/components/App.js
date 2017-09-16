5bf321333bb46d1114402b3564388de6280cca97
Roberto MF <git@virgulilla.com>
2017-09-16 19:31:26 +0200

    Remove boilerplate code

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div id="pokedex-app" className="pk-structure">
+      </div>
+    );
+  }
+}
+
+export default App;
